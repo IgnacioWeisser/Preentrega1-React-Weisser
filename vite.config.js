7693a0cf6e29de773610c7e39844dc20239eb82f
Ignacio Weisser <weisser.89@gmail.com>
2024-05-23 02:42:39 -0400

    Consigna Completa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/Preentrega1-React-Weisser/', // Ruta base según el nombre de tu repositorio
+});
